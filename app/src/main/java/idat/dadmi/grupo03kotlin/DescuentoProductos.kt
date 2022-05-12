863d80c616bb5789cc9443682a6367f9d241c98c
Lenin <leninalbino@gmail.com>
2022-05-12 01:51:29 -0500

    [ruben] ejercicio01 descuento

--- app/src/main/java/idat/dadmi/grupo03kotlin/DescuentoProductos.kt
+++ app/src/main/java/idat/dadmi/grupo03kotlin/DescuentoProductos.kt
@@ -17,13 +17,28 @@ class DescuentoProductos : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        var articulo = binding.txtarticulo.toString().toInt()
-        var monto = binding.txtmontototal.toString().toDouble()
-        //var totalpagar : Double=0.0
-        //var totalpagar2 : Double=0.0
-        //var montotal : String="";
+        var articulo = binding.txtarticulo.text.toString().toInt()
+        var monto = binding.txtmontototal.text.toString().toDouble()
+        var totalpagar : Double=0.0
+        var totalpagar2 : Double=0.0
+        var montotal : String="";
+        if(articulo > 20){
+            totalpagar = monto - (monto * 0.10)
+            montotal = "El total a pagar por haber comprado $articulo articulos es de S/. $totalpagar"
+
+        }
+        if (articulo <= 20 || articulo >=10){
+            totalpagar2 = monto - (monto * 0.05)
+            montotal = "El total a pagar por haber comprado $articulo articulos es de S/. $totalpagar2"
+
+        }
+        if (articulo < 10){
+
+            montotal = "La compra no tiene descuento por haber comprado $articulo articulos y el monto a pagar es S/. $monto"
+
+        }
 
         //Toast.makeText(applicationContext, "Calcular", Toast.LENGTH_LONG).show()
-        binding.tvresultado.text = "$articulo "
+        binding.tvresultado.text = "$montotal "
     }
 }
